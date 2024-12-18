28f5cb3e479b2f6ebad54529429c59a3b01d4c39
MrFrolin <Philip_frolin@hotmail.com>
2024-12-18 15:30:34 +0100

    upd

--- Charlie.Gateway.API/Program.cs
+++ Charlie.Gateway.API/Program.cs
@@ -2,7 +2,7 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddOcelot();
 
