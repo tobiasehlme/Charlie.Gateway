13cd077a5f32aa0205ee6c0677c6d97fe119ac2f
MrFrolin <Philip_frolin@hotmail.com>
2024-12-18 15:25:10 +0100

    upd

--- Charlie.Gateway.API/Program.cs
+++ Charlie.Gateway.API/Program.cs
@@ -1,6 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot();
+
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
 
 app.Run();
